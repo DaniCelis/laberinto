c9bd07fa91da3603b4b18471394fc61241febdf6
DaniCelis <36241903+DaniCelis@users.noreply.github.com>
2018-10-10 09:43:12 -0500

    Update laberinto.py

--- laberinto.py
+++ laberinto.py
@@ -1,4 +1,3 @@
-
 def leer_archivo(archivo):
     return ([x.split(" ") for x in [y.strip("\n") for y in open(archivo).readlines()]])
 
@@ -14,8 +13,42 @@ def coordenadas_finales(archivo):
 def posicion_inicial(coordenadas_iniciales, laberinto):
     if laberinto[coordenadas_iniciales[0]][coordenadas_iniciales[1]] == "1": return False
     return True
+def posicion_final(coordenadas_finales, laberinto):
+    if laberinto[coordenadas_finales[0]][coordenadas_finales[1]] == "1": return False
+    return True
+
+##Aqui se empieza a crear el arbol con las posiciones que se han recorrido del laberinto
+
+def validacion():
+    if posicion_inicial(coordenadas_iniciales("laberinto.txt"), laberinto("laberinto.txt")) == False: return False
+    elif posicion_final(coordenadas_finales("laberinto.txt"), laberinto("laberinto.txt")) == False: return False
+    else:
+        
+
+class ArbolNario:
+    def __init__(self,valor,hijos=[]):
+        self.valor = valor
+        self.hijos = hijos
+
+def agregarANivel(arbol,valor, nivel):
+    if nivel==2:
+        return arbol.hijos.append(ArbolNario(valor))
+    else:
+        return agregarANivel(arbol.hijos[0],valor, nivel-1)
+
+def agregarANodo(arbol,valor, nodo):
+    if nodo==arbol.valor:
+        return arbol.hijos.append(ArbolNario(valor))
+    else:
+        return agregarNodoAux(arbol.hijos,valor,nodo)
+
+def agregarNodoAux(lista,valor,nodo):
+    if lista==[]:
+        return False
+    else:
+        return agregarANodo(lista[0],valor,nodo) or agregarNodoAux(lista[1:],valor,nodo) 
 
 
-print (posicion_inicial(coordenadas_iniciales("laberinto.txt"), laberinto("laberinto.txt")))
 
-#print(coordenadas_iniciales("laberinto.txt"))
+
+#print validacion_final()
